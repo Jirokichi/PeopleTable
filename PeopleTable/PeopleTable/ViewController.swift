58e3a406b76e9d72fba7cb1af5cb2f6aaab00135
kidaG <fx29821@gmail.com>
2016-03-10 22:09:35 +0900

    Added PeopleTable project

---
+++ PeopleTable/PeopleTable/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PeopleTable
+//
+//  Created by yuya on 2016/03/10.
+//  Copyright © 2016年 yuya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
